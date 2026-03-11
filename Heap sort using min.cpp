60bc83e1cc9b2230f268a364a936eb4b80effece
sang98455 <ramana55667@gmail.com>
2026-03-11 11:03:56 +0530

    Add files via upload

---
+++ Heap sort using min.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[], int n, int i)
+{
+    int smallest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if(left < n && arr[left] < arr[smallest])
+        smallest = left;
+
+    if(right < n && arr[right] < arr[smallest])
+        smallest = right;
+
+    if(smallest != i)
+    {
+        swap(arr[i], arr[smallest]);
+        heapify(arr, n, smallest);
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+    for(int i = n/2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for(int i = n-1; i > 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = 6;
+
+    heapSort(arr, n);
+
+    cout << "Sorted array using Min Heap:\n";
+    for(int i=0;i<n;i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
